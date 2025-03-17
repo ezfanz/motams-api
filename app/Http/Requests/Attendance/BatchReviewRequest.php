0b1d1b29bd74f665e7c4a2ddcd0a3083fdc8be78
irfanzulkefly <irfanzulkefly144@gmail.com>
2025-03-18 01:32:36 +0800

    chore - update request validation for Review

--- app/Http/Requests/Attendance/BatchReviewRequest.php
+++ app/Http/Requests/Attendance/BatchReviewRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Attendance;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Status;
+
 
 class BatchReviewRequest extends FormRequest
 {
@@ -20,10 +22,27 @@ class BatchReviewRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'tralasan_id' => 'required|array|min:1',
-            'tralasan_id.*' => 'integer|exists:trans_alasan,id',
-            'status_penyemak' => 'required|integer|in:2,3,6',
-            'catatan_penyemak' => 'nullable|string|max:500',
+            'tralasan_id' => 'required|integer|exists:trans_alasan,id',
+            'status_penyemak' => 'nullable|integer|in:' . implode(',', [
+                Status::MENUNGGU_SEMAKAN,
+                Status::DITERIMA_PENYEMAK,
+                Status::TIDAK_DITERIMA_PENYEMAK,
+                Status::DITERIMA_PENGESAH,
+                Status::TIDAK_DITERIMA_PENGESAH,
+                Status::MEMERLUKAN_MAKLUMAT_LANJUT,
+                Status::PENUKARAN_KAD_ASAL_KE_HIJAU,
+                Status::PENUKARAN_KAD_HIJAU_KE_MERAH,
+                Status::PENUKARAN_KAD_KEKAL_HIJAU,
+                Status::PENUKARAN_KAD_KEKAL_MERAH,
+                Status::PENUKARAN_KAD_MERAH_KE_HIJAU,
+                Status::PENUKARAN_KAD_HIJAU_KE_ASAL,
+                Status::DILULUSKAN_KETUA_JABATAN,
+                Status::TIDAK_DILULUSKAN_KETUA_JABATAN,
+                Status::BARU,
+                Status::DILULUSKAN,
+                Status::TIDAK_DILULUSKAN,
+            ]),
+            'catatan_penyemak' => 'nullable|string|max:255',
         ];
     }
 }
